799c93b325094da45ebf61c8b7d52784f77b9143
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-28 14:34:05 +0500

    first commit from hostel

---
+++ Ch5-P6-ObjectInitializers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_P6_ObjectInitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Fun with Object Init Syntax ***** \n ");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
